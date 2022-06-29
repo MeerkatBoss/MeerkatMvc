e97cb20da8053d302ef73a68f6f0006b34387f44
MeerkatBoss <ivan.solodovnikov@outlook.com>
2022-06-29 15:51:47 +0300

    tests(UserApiService): test Not Found handling
    - New tests for GetUser, UpdateUser and DeleteUser

--- Tests/UserApiServiceTests.cs
+++ Tests/UserApiServiceTests.cs
@@ -190,7 +190,7 @@ public class UserApiServiceTests
 
             Assert.True(userLogIn.HasErrors);
             Assert.AreEqual(1, userLogIn.Errors.Count());
-            Assert.AreEqual(userLogIn.Errors["Detail"], new[] {UserApiService.LOGIN_FAILED});
+            Assert.AreEqual(new[] {UserApiService.LOGIN_FAILED}, userLogIn.Errors["Detail"]);
         }
 
     }
@@ -233,6 +233,40 @@ public class UserApiServiceTests
             Assert.AreEqual(new AuthenticationHeaderValue("Bearer", jwt), sent.Headers.Authorization);
         }
 
+        [Test]
+        public async Task TestGetUserNotFound()
+        {
+            string jwt = "access";
+            var result = new
+            {
+                Type = "Not Found",
+                Title = "Cannot find user",
+                Status = 404,
+                Detail = "No such user",
+            };
+            _messageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                        ItExpr.IsAny<HttpRequestMessage>(),
+                        ItExpr.IsAny<CancellationToken>())
+                .Returns(Task.FromResult(new HttpResponseMessage()
+                        {
+                            StatusCode = HttpStatusCode.NotFound,
+                            Content = JsonContent.Create(result),
+                        }))
+                .Verifiable();
+            byte[] jwtBytes = Encoding.UTF8.GetBytes(jwt);
+            _sessionMock
+                .Setup(x => x.TryGetValue("AccessToken", out jwtBytes!));
+            IUserApiService userApi = new UserApiService(
+                    new HttpClient(_messageHandlerMock.Object){BaseAddress = new Uri(_baseAddress)});
+
+            ProblemModel<UserModel> userGet = await userApi.GetUserAsync(_sessionMock.Object);
+
+            Assert.True(userGet.HasErrors);
+            Assert.AreEqual(1, userGet.Errors.Count());
+            Assert.AreEqual(new[] {UserApiService.NOT_FOUND}, userGet.Errors["Detail"]);
+        }
     }
 
     [TestFixture]
@@ -340,6 +374,7 @@ public class UserApiServiceTests
         [Test]
         public async Task TestUpdateUserWrongPassword()
         {
+            string jwt = "access";
             var model = new UpdateModel(OldPassword: "testtest", Username: "test");
             _messageHandlerMock
                 .Protected()
@@ -351,6 +386,9 @@ public class UserApiServiceTests
                             StatusCode = HttpStatusCode.Unauthorized
                         }))
                 .Verifiable();
+            byte[] jwtBytes = Encoding.UTF8.GetBytes(jwt);
+            _sessionMock
+                .Setup(x => x.TryGetValue("AccessToken", out jwtBytes!));
             IUserApiService userApi = new UserApiService(
                     new HttpClient(_messageHandlerMock.Object){BaseAddress = new Uri(_baseAddress)});
 
@@ -358,7 +396,47 @@ public class UserApiServiceTests
 
             Assert.True(userUpdate.HasErrors);
             Assert.AreEqual(1, userUpdate.Errors.Count());
-            Assert.AreEqual(userUpdate.Errors["Detail"], new[] {UserApiService.WRONG_PASSWORD});
+            Assert.AreEqual(new[] {UserApiService.WRONG_PASSWORD}, userUpdate.Errors["Detail"]);
+            _sessionMock
+                .Verify(x => x.Set("Username", It.IsAny<byte[]>()), Times.Never());
+        }
+
+        [Test]
+        public async Task TestUpdateUserNotFound()
+        {
+            string jwt = "access";
+            var model = new UpdateModel(OldPassword: "testtest", Username: "test");
+            var result = new
+            {
+                Type = "Not Found",
+                Title = "Cannot find user",
+                Status = 404,
+                Detail = "No such user",
+            };
+            _messageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                        ItExpr.IsAny<HttpRequestMessage>(),
+                        ItExpr.IsAny<CancellationToken>())
+                .Returns(Task.FromResult(new HttpResponseMessage()
+                        {
+                            StatusCode = HttpStatusCode.NotFound,
+                            Content = JsonContent.Create(result),
+                        }))
+                .Verifiable();
+            byte[] jwtBytes = Encoding.UTF8.GetBytes(jwt);
+            _sessionMock
+                .Setup(x => x.TryGetValue("AccessToken", out jwtBytes!));
+            IUserApiService userApi = new UserApiService(
+                    new HttpClient(_messageHandlerMock.Object){BaseAddress = new Uri(_baseAddress)});
+
+            ProblemModel<UserModel> userUpdate = await userApi.UpdateUserAsync(_sessionMock.Object, model);
+
+            Assert.True(userUpdate.HasErrors);
+            Assert.AreEqual(1, userUpdate.Errors.Count());
+            Assert.AreEqual(new[] {UserApiService.NOT_FOUND}, userUpdate.Errors["Detail"]);
+            _sessionMock
+                .Verify(x => x.Set("Username", It.IsAny<byte[]>()), Times.Never());
         }
     }
 
@@ -410,6 +488,7 @@ public class UserApiServiceTests
         [Test]
         public async Task TestDeleteUserWrongPassword()
         {
+            string jwt = "access";
             var model = new DeleteModel("testtest");
             _messageHandlerMock
                 .Protected()
@@ -421,6 +500,9 @@ public class UserApiServiceTests
                             StatusCode = HttpStatusCode.Unauthorized
                         }))
                 .Verifiable();
+            byte[] jwtBytes = Encoding.UTF8.GetBytes(jwt);
+            _sessionMock
+                .Setup(x => x.TryGetValue("AccessToken", out jwtBytes!));
             IUserApiService userApi = new UserApiService(
                     new HttpClient(_messageHandlerMock.Object){BaseAddress = new Uri(_baseAddress)});
 
@@ -428,7 +510,47 @@ public class UserApiServiceTests
 
             Assert.True(userDelete.HasErrors);
             Assert.AreEqual(1, userDelete.Errors.Count());
-            Assert.AreEqual(userDelete.Errors["Detail"], new[] {UserApiService.WRONG_PASSWORD});
+            Assert.AreEqual(new[] {UserApiService.WRONG_PASSWORD}, userDelete.Errors["Detail"]);
+            _sessionMock
+                .Verify(x => x.Clear(), Times.Never());
+        }
+
+        [Test]
+        public async Task TestDeleteUserNotFound()
+        {
+            string jwt = "access";
+            var model = new DeleteModel("testtest");
+            var result = new
+            {
+                Type = "Not Found",
+                Title = "Cannot find user",
+                Status = 404,
+                Detail = "No such user",
+            };
+            _messageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                        ItExpr.IsAny<HttpRequestMessage>(),
+                        ItExpr.IsAny<CancellationToken>())
+                .Returns(Task.FromResult(new HttpResponseMessage()
+                        {
+                            StatusCode = HttpStatusCode.NotFound,
+                            Content = JsonContent.Create(result),
+                        }))
+                .Verifiable();
+            byte[] jwtBytes = Encoding.UTF8.GetBytes(jwt);
+            _sessionMock
+                .Setup(x => x.TryGetValue("AccessToken", out jwtBytes!));
+            IUserApiService userApi = new UserApiService(
+                    new HttpClient(_messageHandlerMock.Object){BaseAddress = new Uri(_baseAddress)});
+
+            ProblemModel<UserModel> userDelete = await userApi.DeleteUserAsync(_sessionMock.Object, model);
+
+            Assert.True(userDelete.HasErrors);
+            Assert.AreEqual(1, userDelete.Errors.Count());
+            Assert.AreEqual(new[] {UserApiService.NOT_FOUND}, userDelete.Errors["Detail"]);
+            _sessionMock
+                .Verify(x => x.Clear(), Times.Never());
         }
     }
 
